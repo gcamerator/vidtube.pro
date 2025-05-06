9913e7f6b6ca69f4c55ad7f2f8e249299074d128
Midsoune@24 <165938418+gcamerator@users.noreply.github.com>
2025-05-06 18:21:26 +0100

    Create NodeJS WhatsappBot.js

---
+++ NodeJS WhatsappBot.js
@@ -0,0 +1,11 @@
+// MidSoune
+
+const headers = {
+  'Referer': 'https://vidtube.pro/',
+  'User-Agent': 'Mozilla/5.0',
+};
+const url = "https://vidtube.pro/d/jv5u9xir1v7r.html";
+const res = await VidtubeDownloads(url);
+const res = await fetch(res.directLink[0], { headers });
+const buffer = await res.arrayBuffer();
+await conn.sendMessage(m.chat, {  document:  Buffer.from(buffer),  fileName: 'video.mp4',  mimetype: 'video/mp4',}, { quoted: null });
